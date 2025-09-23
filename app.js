718338876bb88288a152cd204601b1cbf789e9d5
jpaulgb <jepgb@hotmail.com>
2025-09-23 18:19:14 -0500

    se agrega nombre para probar la conexion del DOM, el campo esta vacio, y se captura amigo por id (input)

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
 console.log(amigos);
+
 function agregarAmigo() {
-    document.getElementById(amigo)
+    let nuevoAmigo = document.getElementById(amigo);
+    console.log(nuevoAmigo)
+
+    if ( nuevoAmigo === null) {
+    alert('El campo esta vacio introduzca un nombre para iniciar');
+    }
 };
+
+let nombre = document.getElementById('listaAmigos')
+nombre.innerHTML = "Nombre agregado manualmente TEMPORAL"
+
+
+/* let resultado = document.getElementById('resultado')
+nombre.innerHTML = "El ganador es: nombre agregado manualmente TEMPORAL" */
+
+//validación de que el campo no esta vacio
+
