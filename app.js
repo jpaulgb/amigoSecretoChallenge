7def455a591e8d83987a835634d9b77c41d6bd43
jpaulgb <jepgb@hotmail.com>
2025-09-23 17:56:45 -0500

    function agregar amigo

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = []
-console.log(amigos)
-
+let amigos = [];
+console.log(amigos);
+function agregarAmigo() {
+    document.getElementById(amigo)
+};
